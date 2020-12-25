f1689aea56506988c29f6bcef1c45072b01dba36
Damilare-cloud <fridaydamilare22@gmail.com>
2020-12-25 12:48:31 +0100

    making a counter you can increase also you can decrease and rest

---
+++ main.js
@@ -0,0 +1,25 @@
+count = 0;
+
+const btn = document.querySelectorAll(".btn");
+const value = document.querySelector("#value");
+
+btn.forEach(function(btn){
+    btn.addEventListener("click", function(e){
+        const style = e.currentTarget.classList;
+        if(style.contains("decrease")){
+            count--;
+        }else if(style.contains("increase")){
+            count++;
+        }else{
+            count = 0;
+        }
+        if(count > 0){
+            value.style.color = "green"
+        }else if(count < 0){
+            value.style.color = "red"
+        }else{
+            value.style.color = "black"
+        }
+        value.textContent = count;
+    })
+})
